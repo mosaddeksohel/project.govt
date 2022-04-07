ff03f230aaefac90ef59006a5fde8c127f7c8685
Sohel Tanjim <tanjim.sohel885@gmail.com>
2022-04-07 20:13:39 +0600

    setup firebase

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBX4s_TJ3ZOJE85zhAVjGQdYxYvpfiDP_E",
+  authDomain: "project-sheba.firebaseapp.com",
+  projectId: "project-sheba",
+  storageBucket: "project-sheba.appspot.com",
+  messagingSenderId: "325452466700",
+  appId: "1:325452466700:web:476acd30d29cb143edbc29",
+  measurementId: "G-E2XYLEL314",
+};
+
+export default firebaseConfig;
